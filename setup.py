7cd8d4fa98d8b7774d4c23cbb601bad9c36812e8
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-11-02 18:49:26 -0600

    chore: swap lcov generation

--- setup.py
+++ setup.py
@@ -11,12 +11,11 @@ REQUIREMENTS = [
 DEV_REQUIREMENTS = [
     'black == 22.*',
     'build == 0.7.*',
-    'coveralls == 3.*',
     'flake8 == 4.*',
     'isort == 5.*',
     'mypy == 0.942',
     'pytest == 7.*',
-    'pytest-cov == 3.*',
+    'pytest-cov == 4.*',
     'twine == 4.*',
 ]
 
